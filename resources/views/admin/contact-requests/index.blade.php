c9e2901a165c0b3993face18e92372fb30bddc66
sawsan ghabayen <ghabayensawsan@gmail.com>
2022-12-12 10:25:10 +0200

    message commit

--- resources/views/admin/contact-requests/index.blade.php
+++ resources/views/admin/contact-requests/index.blade.php
@@ -28,6 +28,8 @@
                         <th style="min-width: 300px">Email</th>
                         <th style="min-width: 300px">Message</th>
                         <th style="min-width: 100px">Send Date</th>
+                        <th class="wd-15p"> {{ucwords(__('cp.action'))}}</th>
+
                     </tr>
                 </thead>
                 <tbody>
@@ -56,6 +58,14 @@
                             <span
                                 class="text-dark-75 font-weight-bolder d-block font-size-lg">{{$message->created_at}}</span>
                         </td>
+
+                        <td class="v-align-middle wd-15p optionAddHours">
+                          
+                            <a href="#"  onclick="confirmDelete('{{$message->id}}', this)" role="button" title="{{__('cp.delete')}}" data-toggle="modal" class="btn btn-sm btn-clean btn-icon"><i class="la la-trash"></i></a>
+
+
+
+                        </td>
                     </tr>
                     @endforeach
             </table>
@@ -68,10 +78,46 @@
 @endsection
 
 @section('js')
-<script src="{{asset('assets/js/pages/widgets.js')}}"></script>
+<script src="{{asset('js/axios.js')}}"></script>
+<script src="{{asset('assets/js/pages/features/miscellaneous/toastr.js')}}"></script>
+<script src="{{asset('assets/js/pages/features/miscellaneous/sweetalert2.js')}}"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
-    function performDestroy(id,reference) {
-        confirmDestroy('/cms/admin/messages', id, reference);
+   function confirmDelete(id, reference) {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            cancelButtonColor: '#d33',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+        if (result.isConfirmed) {
+            performDelete(id, reference);
+        }
+        });
+    }
+     function performDelete(id, reference) {
+        axios.delete('/admin/messages/'+id)
+        .then(function (response) {
+            // toastr.success(response.data.message);
+            console.log(response);
+            reference.closest('tr').remove();
+            showMessage(response.data);
+        })
+        .catch(function (error) {
+            console.log(error.response);
+            // toastr.error(error.response.data.message);
+            showMessage(error.response.data);
+        });
+    }
+       function showMessage(data) {
+        Swal.fire(
+            data.title,
+            data.text,
+            data.icon
+        );
     }
 </script>
 @endsection
